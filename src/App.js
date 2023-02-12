e6f29c845ea828ea3071d7088bfac092a2bee188
JohanBedoya <jmbedoya50@gmail.com>
2023-02-12 13:42:58 -0500

    no muchos avances

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import './components/hovereableText';
 
-function App() {
+function Text() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex-container">
+      <div>
+        <br></br>
+      </div>
+      <div>
+      <h1 onMouseDown={"document.getElementById(text1).innerHTML = 'hola';"} id='text1'> Johan </h1>
+      <h1> Bedoya </h1>
+      </div>
     </div>
   );
 }
 
-export default App;
+
+export default Text;
